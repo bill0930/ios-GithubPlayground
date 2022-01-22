77a061ab1fc2f7b7fdff0d779721f7cfcfd0c4f3
Billy Chan <bill0930@gmail.com>
2022-01-22 02:25:33 -0500

    feat: bootstrap the project

---
+++ GithubPlayground/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  GithubPlayground
+//
+//  Created by CHI YU CHAN on 22/1/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
